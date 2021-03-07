bc8be93f5b8c2519ea5c0f5ffc7262be6bc6ee18
Prabhjot Lakhesar <prabh@Prabhjots-MacBook-Pro.local>
2021-03-07 02:45:30 -0500

    Added data cleaning script

---
+++ data_cleaning.py
@@ -0,0 +1,23 @@
+import string 
+import pandas as pd
+
+def remove_punctuation(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+    
+#Questions csv, drop columns
+questions = pd.read_csv('Questions.csv', encoding='latin1')
+questions = questions.drop(['OwnerUserId', 'CreationDate', 'Score', 'Body'], axis = 1)
+
+questions['Questions'] = [remove_punctuation(question) for question in questions['Title']]
+questions = questions.drop(['Title'], axis = 1)
+                            
+#Tags csv
+tags = pd.read_csv('Tags.csv', encoding='latin1')
+
+#Merge the dataframes and drop ID 
+df = pd.merge(questions, tags, on='Id')
+df = df.drop(['Id'], axis=1)
+
+
+df.to_csv('questions_tagged.csv', index=False)
