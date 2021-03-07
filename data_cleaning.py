24b1e5271a512d8573374659bb011af01b028ab5
Prabhjot Lakhesar <prabh@Prabhjots-MacBook-Pro.local>
2021-03-07 03:27:57 -0500

    Updated data cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,14 +2,17 @@ import string
 import pandas as pd
 
 def remove_punctuation(text):
-        exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
-    
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in text if ch not in exclude)
+def add_periods(text):
+    text = text + '.'
+    return text
 #Questions csv, drop columns
 questions = pd.read_csv('Questions.csv', encoding='latin1')
 questions = questions.drop(['OwnerUserId', 'CreationDate', 'Score', 'Body'], axis = 1)
 
 questions['Questions'] = [remove_punctuation(question) for question in questions['Title']]
+questions['Questions'] = [add_periods(question) for question in questions['Questions']]
 questions = questions.drop(['Title'], axis = 1)
                             
 #Tags csv
