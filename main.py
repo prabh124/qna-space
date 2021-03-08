3c37736924193633f56829b688c8ed8fcfabc448
Prabhjot Lakhesar <prabh@Prabhjots-MacBook-Pro.local>
2021-03-08 01:32:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from questions import Questions
-import pandas as pd 
 
 if __name__ == '__main__':
+    
     q = Questions()
     df = q.initDataframe()
     
-    q.askQuestion(df, 'How can I take the derivative of x?', q.getStudentNum())
+    q.askQuestion(df, 'How does the len() function work?', q.getStudentNum(), 24)
+    q.askQuestion(df, 'How do I add a list to a dataframe?', q.getStudentNum())
     
-    q.askQuestion(df, 'How can I take the derivative of y?', q.getStudentNum(), 10)
+    q.askQuestion(df, 'How can I find the sum of a list?', q.getStudentNum(), 4)
+    q.askQuestion(df, 'What is abstraction?', q.getStudentNum(), 2)
     
-    q.askQuestion(df, 'How can I take the integral of y?', q.getStudentNum())
+    
+    q.askQuestion(df, 'What are the 4 pillars of Object Oriented Programming?', q.getStudentNum(), 4)
+    q.askQuestion(df, 'How do you compare a list of characters?', q.getStudentNum(), 2)
     
     studentNums = df['studentNum'].tolist()
     
@@ -19,6 +23,3 @@ if __name__ == '__main__':
     q.upvoteQuestion(df, studentNums[1])
     
     print(df)
-    
-    #df2 = pd.read_csv('Questions2.csv', encoding='latin1')
-    
